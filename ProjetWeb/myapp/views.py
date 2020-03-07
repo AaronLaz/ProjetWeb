85d0d7c5f166403465eeab3eef3125c8b7066b5a
Aaron <aaron.lazaroo@etu.umontpellier.fr>
2020-03-07 23:37:42 +0100

    Page numbering

--- ProjetWeb/myapp/views.py
+++ ProjetWeb/myapp/views.py
@@ -9,6 +9,9 @@ from account.models import Account
 from blog.models import BlogPost
 from operator import attrgetter
 from blog.views import get_blog_queryset
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+BLOG_POSTS_PER_PAGE = 10
 
 # Create your views here.
 
@@ -20,12 +23,25 @@ def homepage(request):
 
 	query = ""
 	if request.GET:
-		query = request.GET['q']
+		query = request.GET.get('q','') # get value of query, default is empty
 		context['query'] = str(query)
 
 	# Sort all blog posts by latest date
 	blog_posts = sorted(get_blog_queryset(query), key=attrgetter('date_updated'),reverse=True)
 	context['blog_posts'] = blog_posts
+
+	# Page numbers
+	page = request.GET.get('page',1) #default number is 1
+	blog_posts_pagenum = Paginator(blog_posts, BLOG_POSTS_PER_PAGE)
+
+	try:
+		blog_posts = blog_posts_pagenum.page(page)
+	except PageNotAnInteger:	
+		blog_posts = blog_posts_pagenum.page(BLOG_POSTS_PER_PAGE)
+	except EmptyPage:
+		blog_posts = blog_posts_pagenum.page(blog_posts_pagenum.num_pages)
+
+	context['blog_posts'] = blog_posts	
 
 	return render(request,'homepage.html',context)	
 
