d49390db18e3be4d12553b24e94dd981efcd8bc1
Aaron <aaron.lazaroo@etu.umontpellier.fr>
2020-03-08 00:47:58 +0100

    heroku settings directory

--- ProjetWeb/ProjetWeb/wsgi.py
+++ ProjetWeb/ProjetWeb/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjetWeb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjetWeb.ProjetWeb.settings')
 
 application = get_wsgi_application()
