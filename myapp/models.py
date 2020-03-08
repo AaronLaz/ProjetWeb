507ab817ad3d48f9e60bf2a441be363797febdf7
Aaron <aaron.lazaroo@etu.umontpellier.fr>
2020-03-08 12:47:20 +0100

    Scramble object instances

--- myapp/models.py
+++ myapp/models.py
@@ -3,17 +3,24 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-PUZZLE = [
+# Puzzle types, from 3x3 to 5x5
+class Puzzle(models.Model):
+    PUZZLE = [
     ("2","2x2"),
     ("3","3x3"),
     ("4","4x4"),
     ("5","5x5")
     ]
 
-class Scramble(models.Model):
-	scrambleID = models.AutoField(primary_key=True)
-	scrambleLength = models.IntegerField(blank=False)
-	scramble = models.CharField(blank=True, max_length=45)
+    puzzleID = models.AutoField(primary_key=True)
+    puzzleType = models.CharField(blank=True,max_length=1,choices=PUZZLE)
 
-	class Meta:
-		db_table = 'scramble'
+    def __str__(self):
+        return self.puzzleType+ "x" + self.puzzleType 
+
+
+# Scrambles in relation to the puzzle type
+class Scramble(models.Model):
+    scrambleID = models.AutoField(primary_key = True)
+    scrambleType = models.ForeignKey(Puzzle, on_delete=models.CASCADE,blank=True,null=True  )
+    scramble = models.CharField(blank=True, max_length=45)
--- myapp/views.py
+++ myapp/views.py
@@ -11,6 +11,7 @@ from operator import attrgetter
 from blog.views import get_blog_queryset
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from pyTwistyScrambler import scrambler222, scrambler333, scrambler444, scrambler555
+import random
 
 BLOG_POSTS_PER_PAGE = 10
 
@@ -51,16 +52,29 @@ def scramble(request):
 
 def scrambler(request, type):
 	context ={}
-	if type==2:
-		scramble = scrambler222.get_WCA_scramble()
-	elif type==4:
-		scramble = scrambler444.get_WCA_scramble()
-	elif type==5:
-		scramble = scrambler555.get_WCA_scramble()
-	else: # default is 3x3
-		scramble = scrambler333.get_WCA_scramble() 
 
-	context['scramble'] = scramble				
+	puzzle = Puzzle.objects.get(puzzleType=type)	 
+
+	list_scrambles = Scramble.objects.all().filter(scrambleType=puzzle)
+	if list_scrambles.count()<15:
+		if type==2:
+			strtype="2"
+			scramble = scrambler222.get_WCA_scramble()
+		elif type==4:
+			strtype="4"
+			scramble = scrambler444.get_WCA_scramble()
+		elif type==5:
+			strtype="5"
+			scramble = scrambler555.get_WCA_scramble()
+		else: # default is 3x3
+			strtype="3"
+			scramble = scrambler333.get_WCA_scramble()
+
+		newscramble = Scramble(scrambleType=puzzle,scramble=scramble)
+		context['scramble'] = newscramble
+		newscramble.save()
+	else:
+		context['scramble'] = random.choice(list_scrambles)				
 
 	return render(request,'scramble.html',context)		
 
